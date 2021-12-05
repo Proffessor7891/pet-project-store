922f1c188b0c16077e9442166e72b5dfa42d7a09
Антон <84575959+Proffessor7891@users.noreply.github.com>
2021-12-05 15:46:56 +0300

    Update index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -14,17 +14,3 @@ export default rootReducer;
 
 
 
-/*
-import { combineReducers } from 'redux';
-
-import filters from './filters';
-import pizzas from './pizzas';
-import cart from './cart';
-
-const rootReducer = combineReducers({
-  filters,
-  pizzas,
-  cart,
-});
-
-export default rootReducer;*/
