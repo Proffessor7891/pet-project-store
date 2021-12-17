2d716de1fae275dd86f8a7e6f67f676a2bd01db8
Антон <84575959+Proffessor7891@users.noreply.github.com>
2021-12-17 15:57:43 +0300

    var

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+
+import style from './header.module.scss';
 import './header.module.scss';
+
 import logoSvg from '../assets/img/pizza-logo.svg';
 import Button from './Button';
 
